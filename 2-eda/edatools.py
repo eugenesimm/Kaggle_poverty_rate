18147b8f3b0af4989b6c28c84e04067fbe793832
jasonleejs <jasonleejs12345@gmail.com>
2024-11-17 17:21:54 -0500

    correlations with rating

--- 2-eda/edatools.py
+++ 2-eda/edatools.py
@@ -15,3 +15,26 @@ def correlational_matrix_heatmap(data, fig_size=(6,6), cols_to_exclude=[]):
     ax.xaxis.set_label_position('top')  # Sets the label position to the top
 
     plt.show()
+
+
+def correlation_bar_graph(data, y_col='subjectivePoverty_rating', fig_size=(20, 8), x_cols_to_exclude=[]):
+
+
+    # Assuming 'correlations' is a Series containing correlations with 'rating'
+    # Example: correlations = merged_df.corr()['rating'].sort_values(ascending=False)
+    cols = [col for col in data.columns if col not in x_cols_to_exclude]
+    correlations = data[cols].corr()[y_col].abs().sort_values(ascending=False)
+    correlations = correlations[correlations.index != 'subjectivePoverty_rating']
+
+    # Plot a bar graph
+    plt.figure(figsize=fig_size)
+    correlations.plot(kind='bar', color='skyblue', edgecolor='black')
+    plt.title(f'Absolute Value of Correlations with {y_col}', fontsize=16)
+    plt.xlabel('Explanatory variables', fontsize=14)
+    plt.ylabel(f'Correlation with {y_col}', fontsize=14)
+    plt.xticks(rotation=45, ha='right', fontsize=12)
+    plt.grid(axis='y', linestyle='--', alpha=0.7)
+    plt.tight_layout()
+
+    # Show the plot
+    plt.show()
